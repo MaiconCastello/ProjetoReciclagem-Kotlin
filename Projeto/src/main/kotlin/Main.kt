532c27a46a36d2c017da9a081b0766a25ebb424c
MaiconCastello <maicon.kastello@gmail.com>
2022-05-27 10:34:11 -0300

    Menu principal

--- Projeto/src/main/kotlin/Main.kt
+++ Projeto/src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 fun main() {
 
-    println("Oi Pessoal")
+    println("Bem vinde, selecione uma das opções abaixo:")
+    println("MENU" +
+            "\n1 → Verificar se um produto é reciclável ou não" +
+            "\n2 → Onde Descartar materiais específicos" +
+            "\n3 → Calculadora de valores com reciclagem" +
+            "\n4 → Sair")
+
 
 }
