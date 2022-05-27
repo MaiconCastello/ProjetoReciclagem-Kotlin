3164b63cfcddbffd44cde9747c23bdbc6ce10eb2
lizandra <lizandra.ferrari@hotmail.com>
2022-05-27 16:02:18 -0300

    descarte

--- Projeto/src/main/kotlin/Descarte.kt
+++ Projeto/src/main/kotlin/Descarte.kt
@@ -1,15 +1,53 @@
 object Descarte {
-    fun pilhas(){
+    fun pilhas() {
         println("\nAs pilhas e baterias devem ser entregues às lojas que comercializam ou às assistências técnicas autorizadas para que eles entreguem aos fabricantes.\n")
-        println("ATACADÃO MATRIZ AV. MORVAN DIAS DE FIGUEIREDO, 6169 VILA MARIA\n" +
-                "ATACADÃO JACUI AVENIDA DOUTOR CUSTÓDIO DE LIMA, s/n PARQUE CRUZEIRO DO SUL\n" +
-                "ATACADÃO IPIRANGA RUA DO MANIFESTO, 931 IPIRANGA\n" +
-                "ATACADÃO SANTO AMARO Estrada do M Boi Mirim, 380 Jardim das Flores\n" +
-                "ATACADÃO ARICANDUVA Av. ARICANDUVA, 3640 VILA CALIFORNIA\n" +
-                "ATACADÃO INTERLAGOS Avenida Interlagos, 4211 Jardim Umuarama\n" +
-                "ATACADÃO INAJAR DE SOUZA Avenida Inajar de Souza, 5180 Vila Rica\n" +
-                "ATACADÃO ANCHIETA Rua MARGINAL DIREITA ANCHIETA, S/N JARDIM SANTA CRUZ\n" +
-                "ATACADÃO SÃO MIGUEL Av. Marechal Tito, 2501 S. Miguel Paulista\n")
+        println(
+            "ATACADÃO MATRIZ AV. MORVAN DIAS DE FIGUEIREDO, 6169 VILA MARIA\n" +
+                    "ATACADÃO JACUI AVENIDA DOUTOR CUSTÓDIO DE LIMA, s/n PARQUE CRUZEIRO DO SUL\n" +
+                    "ATACADÃO IPIRANGA RUA DO MANIFESTO, 931 IPIRANGA\n" +
+                    "ATACADÃO SANTO AMARO Estrada do M Boi Mirim, 380 Jardim das Flores\n" +
+                    "ATACADÃO ARICANDUVA Av. ARICANDUVA, 3640 VILA CALIFORNIA\n" +
+                    "ATACADÃO INTERLAGOS Avenida Interlagos, 4211 Jardim Umuarama\n" +
+                    "ATACADÃO INAJAR DE SOUZA Avenida Inajar de Souza, 5180 Vila Rica\n" +
+                    "ATACADÃO ANCHIETA Rua MARGINAL DIREITA ANCHIETA, S/N JARDIM SANTA CRUZ\n" +
+                    "ATACADÃO SÃO MIGUEL Av. Marechal Tito, 2501 S. Miguel Paulista\n")
+        print("Aperte enter para continuar...")
+        readln()
+        println()
+    }
+
+    fun eletronicos() {
+        println(
+            "Dispositivos eletroeletrônicos devem ser descartados de forma correta devido ao " +
+                    "seus componentes metálicos. Confira alguns endereços de descarte na grande SP:"
+        )
+        println(
+            "\nCarrefour Santo André Avenida Pedro Américo, 23 – Vila Humaita Alameda Terracota, 545 – Cerâmica" +
+                    "\nMakro São Bernardo do Campo Rua Frei Damião, 355 – Vila Jordanopolis" +
+                    "\nCasas Bahia São Bernardo do Campo – Rudge Ramos Avenida Senador Vergueiro, 5058 – Rudge Ramos" +
+                    "\nMakro Santo André Avenida dos Estados, 7439 – Tamanduateí 6" +
+                    "\nKalunga Santo André – ABC Plaza Avenida Industrial, 600 – Jardim" +
+                    "\nBig São Bernardo do Campo Rua Marechal Deodoro, 2785 – Centro Praça Samuel Sabatini, 50 – Centro" +
+                    "\nParkShoppingSãoCaetano Alameda Terracota, 545 – Cerâmica")
+        print("Aperte enter para continuar...")
+        readln()
+        println()
+
+    }
+
+    fun oleodecozinha() {
+        println(
+            "Óleo de cozinha pode poluir a agua se descartado de forma incorreta." +
+                    " Confira alguns pontos de descarte em SP: "
+        )
+        println(
+            "\nMERCADO MUNICIPAL DE SAPOPEMBA AV: SAPOPEMBA, 7911" +
+                    "\nMERCADO MUNICIPAL DE PIRITUBA RUA ALMIRANTE ISAIAS DE NORONHA, 163" +
+                    "\nSACOLÃO MUNICIPAL PIRAPORINHA RUA MANOEL DIAS LEME, 55" +
+                    "\nSACOLÃO MUNICIPAL BRIGADEIRO RUA 13 DE MAIO X BRIG. LUIZ ANTONIO" +
+                    "\nSACOLÃO MUNICIPAL REAL PARQUE RUA CONDE DE ITAGUAÍ E PAULO BOURROL, 120" +
+                    "\nSACOLÃO MUNICIPAL DA LAPA RUA ARISTIDES VIADANA, S/N" +
+                    "\nMERCADO MUNICIPAL PAULISTANO RUA DA CANTAREIRA, 306 ")
         print("Aperte enter para continuar...")
         readln()
         println()
