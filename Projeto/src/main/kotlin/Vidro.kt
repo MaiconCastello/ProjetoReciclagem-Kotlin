92b8e26d5bf63f645dec6ff2135724c4b3725243
Leticia Almeida <lehzlucis@gmail.com>
2022-05-27 13:59:19 -0300

    AtualizacaoVidro

--- Projeto/src/main/kotlin/Vidro.kt
+++ Projeto/src/main/kotlin/Vidro.kt
@@ -1,7 +1,22 @@
-abstract class Vidro {
-    var vidro = mutableListOf<String> ()
+class vidro(reciclaveis: Boolean, noscivo:Boolean): Materiais(reciclaveis, noscivo){
 
-fun reciclar(){
-println("hehe")
+    override var reciclável: String = ("Potes de conservas, Embalagens, Frascos de remédios vazios, Copos, Garrafas\n")
+    override var nreciclável: String = ("Espelhos, Boxes temperados, Louças, Óculos, Cerâmicas, Porcelanas, Pirex, " +
+                                         "Tubos de TV,  Monitores, Para-brisa de carros ")
 }
+
+fun calculadora() {
+
+    println("Escolha o tipo de vidro:"+
+            "\n1 - Ambar"+
+            "\n2 - Transparente"+
+            "\n 3 - Verde")
+    var opc = readln().toInt()
+
+    when(opc){
+        1-> precokg = x
+        2-> precokg = y
+        3-> precokg  = z}
+
+
 }
